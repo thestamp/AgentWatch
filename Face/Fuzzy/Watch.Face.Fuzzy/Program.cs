3c3770b1ccb3063043f30fcf95943416345aac72
Jeff <thestamp@gmail.com>
2013-06-16 23:11:37 -0500

    Added Fuzzy Watch Face

---
+++ Face/Fuzzy/Watch.Face.Fuzzy/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Input;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Watch.Face.Common;
+using Watch.Face.Fuzzy.WatchFace;
+
+namespace Watch.Face.Fuzzy
+{
+    public class Program : Microsoft.SPOT.Application
+    {
+
+
+        static Timer _updateClockTimer;
+
+        static object _updateTimeLock = new object();
+
+        static DateTime _startTime = new DateTime(2013, 01, 01, 09, 00, 00);
+        private static WatchFaceBase watchFace;
+
+        public static void Main()
+        {
+            // optionally set time; comment out the following line to use the current system time
+            //Microsoft.SPOT.Hardware.Utility.SetLocalTime(_startTime);
+
+            // display the time immediately
+            UpdateTime(null);
+
+            // set up timer to refresh time every minute
+            var currentTime = DateTime.Now;
+            var dueTime = new TimeSpan(0, 0, 0, 59 - currentTime.Second, 1000 - currentTime.Millisecond); // beginning of next minute
+            var period = new TimeSpan(0, 0, 1, 0, 0); // update time every minute
+            _updateClockTimer = new Timer(UpdateTime, null, dueTime, period); // start our minute timer
+
+            // go to sleep; time updates will happen automatically every minute
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void UpdateTime(object state)
+        {
+
+            watchFace = new WatchFaceFuzzy();
+            watchFace.DrawWatchFace();
+
+        }
+
+    }
+}
---
+++ Face/Fuzzy/Watch.Face.Fuzzy/WatchFace/WatchFaceFuzzy.cs
@@ -0,0 +1,180 @@
+using System;
+using Microsoft.SPOT.Presentation.Media;
+using Watch.Face.Common;
+
+namespace Watch.Face.Fuzzy.WatchFace
+{
+    class WatchFaceFuzzy:WatchFaceBase
+    {
+
+        private string GetRandomSmall(bool before)
+        {
+           
+            if (before)
+            {
+                switch (new Random(DateTime.Now.Millisecond).Next(4))
+                {
+                    case 0:
+                        return "just before";
+                    case 1:
+                        return "a little before";
+                    case 2:
+                        return "a pinch before";
+                    case 3:
+                        return "almost";
+                    default:
+                        return "easing into";
+                }
+
+            }
+            else
+            {
+                switch (new Random(DateTime.Now.Millisecond).Next(4))
+                {
+                    case 0:
+                        return "just past";
+                    case 1:
+                        return "a little past";
+                    case 2:
+                        return "a pinch past";
+                    case 3:
+                        return "just after";
+                    default:
+                        return "slipping past";
+                }
+            }
+        
+        }
+
+        private string GetTen(bool before)
+        {
+            return before ? "ten to": "Ten after";
+        }
+
+        private string GetHalfPast()
+        {
+            return "half past";
+        }
+
+        private string GetHour(DateTime time)
+        {
+            switch (time.Hour)
+            {
+                case 0:
+                    return "midnight";
+                case 1:
+                case 13:
+                    return "one";
+                case 2:
+                case 14:
+                    return "two";
+                case 3:
+                case 15:
+                    return "three";
+                case 4:
+                case 16:
+                    return "four";
+                case 5:
+                case 17:
+                    return "five";
+                case 6:
+                case 18:
+                    return "six";
+                case 7:
+                case 19:
+                    return "seven";
+                case 8:
+                case 20:
+                    return "eight";
+                case 9:
+                case 21:
+                    return "nine";
+                case 10:
+                case 22:
+                    return "ten";
+                case 11:
+                case 23:
+                    return "eleven";
+                case 12:
+                default:
+                    return "noon";
+            }
+          
+        }
+
+
+        private string[] GetFuzzyText(DateTime time)
+        {
+            var minuteStart = "";
+            var minuteMiddle = "";
+            var minuteEnd = "";
+
+            if (time.Minute < 4)
+                minuteEnd = "o'clock";
+            if (time.Minute < 9)
+            {
+                minuteMiddle = GetRandomSmall(false);
+                minuteEnd = "o'clock";
+            }
+            else if (time.Minute < 13)
+                minuteMiddle = GetTen(false);
+            else if (time.Minute < 20)
+                minuteMiddle = "quarter past";
+            else if (time.Minute < 27)
+            {
+                minuteStart = GetRandomSmall(true);
+                minuteMiddle = GetHalfPast();
+            }
+            else if (time.Minute < 34)
+                minuteMiddle = GetHalfPast();
+            else if (time.Minute < 41)
+            {
+                minuteStart = GetRandomSmall(false);
+                minuteMiddle = GetHalfPast();
+            }
+            else
+            {
+                if (time.Minute < 48)
+                    minuteMiddle = "quarter to";
+                else if (time.Minute < 53)
+                    minuteMiddle = GetTen(true);
+                else if (time.Minute < 57)
+                {
+                    minuteMiddle = GetRandomSmall(true);
+                    minuteEnd = "o'clock";
+                }
+                else
+                    minuteEnd = "o'clock";
+
+                time = time.AddHours(1);
+            }
+
+            string hour = GetHour(time);
+
+            //return new string[4] { "a little before", "two", "three", "four" };
+            return new[]{minuteStart, minuteMiddle, hour, minuteEnd};
+        }
+
+
+
+        public override void DrawWatchFace()
+        {
+            base.ClearWatchFace();
+   
+            var minuteFont = Resources.GetFont(Resources.FontResources.SegoeUi16);
+            var hourFont = Resources.GetFont(Resources.FontResources.SegoeUi20);
+            var currentTime = DateTime.Now.ToLocalTime();
+            var strings = GetFuzzyText(currentTime);
+
+            const int yOffset = 5;
+            const int xOffset = 2;
+
+            bitmap.DrawText(strings[0], minuteFont, Color.White, xOffset, 0 + yOffset);
+            bitmap.DrawText(strings[1], minuteFont, Color.White, xOffset, 25 + yOffset);
+            bitmap.DrawText(strings[2], hourFont, Color.White, xOffset, 50 + yOffset);
+            bitmap.DrawText(strings[3], minuteFont, Color.White, xOffset, 82 + yOffset);
+
+ 	        base.DrawWatchFace();
+        }
+    }
+}
